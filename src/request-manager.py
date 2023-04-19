3a3a233213424913ae66ec2950851766dca923b1
jpcampos <jpcampos@inegi.up.pt>
2023-04-19 15:04:36 +0000

    Fix multiline stdout/stderr

--- src/request-manager.py
+++ src/request-manager.py
@@ -2,8 +2,11 @@
 
 import os, subprocess, json, time
 from datetime import datetime
-from office365.runtime.auth.user_credential import UserCredential
-from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.user_credential import UserCredential  # type: ignore
+from office365.sharepoint.client_context import ClientContext  # type: ignore
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def message(msg):
@@ -29,7 +32,8 @@ def parse_items(items):
     new, old = [], []
 
     for item in items:
-        if not item['Active']: continue
+        if not item['Active']:
+            continue
         todate = datetime.strptime(item['To'], '%Y-%m-%dT%H:%M:%SZ').date()
         fromdate = datetime.strptime(item['From'], '%Y-%m-%dT%H:%M:%SZ').date()
         now = datetime.today().date()
@@ -67,20 +71,28 @@ try:
         new, old = parse_items(items)
 
         for item in new:
-            res = subprocess.run('bash vm-create.sh ' + ' '.join(item), shell=True, capture_output=True)
+            res = subprocess.run(
+                'bash vm-create.sh ' + ' '.join(item), shell=True, capture_output=True
+            )
             out, err = res.stdout.decode(), res.stderr.decode()
-            if out: message(out)
-            if err: message('ERR ' + err)
+            for line in out.splitlines():
+                message(line)
+            for line in err.splitlines():
+                message('ERR ' + line)
 
         for item in old:
-            res = subprocess.run('bash vm-destroy.sh ' + item, shell=True, capture_output=True)
+            res = subprocess.run(
+                'bash vm-destroy.sh ' + item, shell=True, capture_output=True
+            )
             out, err = res.stdout.decode(), res.stderr.decode()
-            if out: message(out)
-            if err: message('ERR ' + err)
+            for line in out.splitlines():
+                message(line)
+            for line in err.splitlines():
+                message('ERR ' + line)
 
         if new or old:
             message(f'created {len(new)} and destroyed {len(old)}')
-            
+
 except Exception as ex:
     message(ex)
     message('Exiting')
