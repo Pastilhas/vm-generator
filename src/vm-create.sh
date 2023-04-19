df004d4320c063f2561fef990d77667fc9756d9c
jpcampos <jpcampos@inegi.up.pt>
2023-04-19 15:03:16 +0000

    Add exit function

--- src/vm-create.sh
+++ src/vm-create.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/bash
 
+function exit_message {
+    if [ -n "${1}" ]; then
+        echo "${1}" >&2
+    else
+        echo 'Failure while creating machine'
+    fi
+
+    if [ -n "${name}" ]; then
+        virsh undefine "${name}"
+
+        for disk in "${used_disks[@]}"; do
+            rm "${disk}${name}.qcow2"
+        done
+    fi
+
+    if [ -n "${2}" ]; then
+        exit "${2}"
+    else
+        exit 255
+    fi
+}
+
 original_vm='original_vm'
 disk_size='1.7T'
 
@@ -13,8 +35,7 @@ gpus_file="${root_path}gpus_file"
         ${2} =~ ^[0-9]+$ && ${3} =~ ^[0-9]+$ &&
         ${4} =~ ^[0-9]+$ && ${5} =~ ^[0-9]+$ ]]
 } || {
-    echo Incorrect arguments "${@}"
-    exit 1
+    exit_message "Incorrect arguments ${*}" 1
 }
 
 name=${1}
@@ -29,20 +50,16 @@ read -ra avail_disks <${disks_file}
 alph=(echo {b..z})
 date=$(date -u +"%Y-%m-%d %H:%M:%S")
 
-[[ ${gpus} > ${#avail_gpus[@]} || ${disks} > ${#avail_disks[@]} ]] && echo Overloading resources >&2 && exit 2
+[[ ${gpus} > ${#avail_gpus[@]} || ${disks} > ${#avail_disks[@]} ]] &&
+    exit_message 'Overloading resources' 2
 
 {
-    virt-clone --original ${original_vm} --name "${name}" --file "${avail_disks[0]}${name}.qcow2" &&
+    virt-clone --original ${original_vm} --name "${name}" --file "${avail_disks[0]}${name}.qcow2" &>'/dev/null' &&
         used_disks=("${avail_disks[0]}") &&
         avail_disks=("${avail_disks[@]:1}")
 } || {
-    echo Failed cloning original machine >&2
-    virsh undefine "${name}"
-    rm "${used_disks[@]}"
-    exit 3
+    exit_message 'Failed cloning original machine' 3
 }
-
-echo Cloned original vm
 
 {
     virsh setvcpus "${name}" "${cpus}" --maximum --config &&
@@ -50,13 +67,8 @@ echo Cloned original vm
         virsh setmaxmem "${name}" "${mem}G" --config &&
         virsh setmem "${name}" "${mem}G" --config
 } || {
-    echo Failed setting cpus and memory >&2
-    virsh undefine "${name}"
-    rm "${used_disks[@]}"
-    exit 4
+    exit_message 'Failed setting cpus and memory' 4
 }
-
-echo Set vcpus and mem
 
 for _ in $(seq "${gpus}"); do
     {
@@ -64,31 +76,21 @@ for _ in $(seq "${gpus}"); do
             used_gpus=("${used_gpus[@]}" "${avail_gpus[0]}") &&
             avail_gpus=("${avail_gpus[@]:1}")
     } || {
-        echo Failed attaching the gpus >&2
-        virsh undefine "${name}"
-        rm "${used_disks[@]}"
-        exit 5
+        exit_message 'Failed attaching the gpus' 5
     }
 done
 
-echo Attached all gpus
-
 for _ in $(seq "${disks}"); do
     {
-        qemu-img create -f qcow2 -o preallocation=metadata "${avail_disks[0]}${name}.qcow2" ${disk_size} &&
+        qemu-img create -f qcow2 -o preallocation=metadata "${avail_disks[0]}${name}.qcow2" ${disk_size} &>'/dev/null' &&
             virsh attach-disk "${name}" "${avail_disks[0]}${name}.qcow2" "vd${alph[0]}" --cache none --subdriver qcow2 --config &&
             used_disks=("${used_disks[@]}" "${avail_disks[0]}") &&
             avail_disks=("${avail_disks[@]:1}") &&
             alph=("${alph[@]:1}")
     } || {
-        echo Failed attaching the gpus >&2
-        virsh undefine "${name}"
-        rm "${used_disks[@]}"
-        exit 6
+        exit_message 'Failed attaching the gpus' 6
     }
 done
-
-echo Attached all disks
 
 echo "${avail_gpus[@]}" >${gpus_file}
 echo "${avail_disks[@]}" >${disks_file}
@@ -102,4 +104,4 @@ echo "${avail_disks[@]}" >${disks_file}
     echo "${used_disks[@]}"
 } >"${state_dir}${name}"
 
-echo Created "${name}"
+echo "Created ${name}"
