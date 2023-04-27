23202bae4973b9defcb1197f0351e3a83b8beb84
jpcampos <jpcampos@inegi.up.pt>
2023-04-27 08:50:32 +0000

    Refactor python script

--- src/request-manager.py
+++ src/request-manager.py
@@ -8,12 +8,14 @@ from office365.runtime.auth.user_credential import UserCredential  # type: ignor
 from office365.sharepoint.client_context import ClientContext  # type: ignore
 
 
-if subprocess.run('grep "root.*request-manager.py" <<< $(ps -fA)', shell=True, capture_output=True).stdout.strip(): exit(0)
+def call(command: str) -> subprocess.CompletedProcess: return subprocess.run(command, shell=True, capture_output=True)
+
+if call('grep "root.*request-manager.py" <<< $(ps -fA)').stdout.strip(): exit(0)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 CPU_PER_GPU = 7
-RAM_STACK   = 32
+RAM_STACK = 32
 
 
 (sp_user, sp_pass, sp_list_name, sp_url) = ('', '', '', '')
@@ -50,25 +52,24 @@ def parse_items(items):
         todate = datetime.strptime(item['To'], '%Y-%m-%dT%H:%M:%SZ').date()
         fromdate = datetime.strptime(item['From'], '%Y-%m-%dT%H:%M:%SZ').date()
 
-        exists = subprocess.run(f'virsh list | grep -o {name}', shell=True, capture_output=True).stdout.decode().strip()
-
+        exists = call(f'virsh list | grep -o {name}').stdout.decode().strip()
         if not exists and fromdate <= now and todate >= now:
-            changes.append(([
-                'bash',
-                'vm-create.sh',
+            changes.append((
+                f'bash vm-create.sh {name} \
+                    {item["GPUS"] * CPU_PER_GPU} \
+                    {item["RAM_x0028_32GB_x0029_"] * RAM_STACK} \
+                    {item["GPUS"]} \
+                    {item["Storage_x0028_2TB_x0029_"]}',
+                item['UserId'],
                 name,
-                str(item['GPUS'] * CPU_PER_GPU),
-                str(item['RAM_x0028_32GB_x0029_'] * RAM_STACK),
-                str(item['GPUS']),
-                str(item['Storage_x0028_2TB_x0029_']),
-            ], item['UserId'], name))
+            ))
 
         elif exists and todate < now:
-            changes.append(([
-                'bash',
-                'vm-destroy.sh',
-                name,
-            ], item['UserId'], ''))
+            changes.append((
+                f'bash vm-create.sh {name}',
+                item['UserId'],
+                '',
+            ))
 
     return changes
 
@@ -82,9 +83,9 @@ def send_email(subject, body, userid):
     user = user[0].properties
 
     mimemsg = MIMEMultipart()
-    mimemsg["From"] = sp_user
-    mimemsg["To"] = user["Email"]
-    mimemsg["Subject"] = subject
+    mimemsg['From'] = sp_user
+    mimemsg['To'] = user['Email']
+    mimemsg['Subject'] = subject
     mimemsg.attach(MIMEText(body, 'plain'))
 
     connection = smtplib.SMTP(host='smtp.office365.com', port=587)
@@ -95,8 +96,8 @@ def send_email(subject, body, userid):
 
 
 def create_email(name, userid):
-    mac = subprocess.run(f'virsh dumpxml {name} | grep -o ..:..:..:..:..:..', shell=True, capture_output=True).stdout.decode().strip()
-    ip = subprocess.run(f'dhcp-lease-list | grep -Po \"(?<={mac}  ).*?(?= )\"', shell=True, capture_output=True).stdout.decode().strip()
+    mac = call(f'virsh dumpxml {name} | grep -o ..:..:..:..:..:..').stdout.decode().strip()
+    ip = call(f'dhcp-lease-list | grep -Po "(?<={mac}  ).*?(?= )"').stdout.decode().strip()
     if not mac or not ip: raise Exception('Failed to find machine mac and ip')
 
     send_email('Request accepted', f'''
@@ -111,6 +112,7 @@ For example, run this command in your preferred terminal:
 ssh vm@{ip}
 ''', userid)
 
+
 def destroy_email(userid):
     send_email('Access revoked', f'''
 Your reservation for a machine has reached its end.
@@ -120,26 +122,26 @@ Access to it has been revoked.
 
 try:
     while True:
-        time.sleep(5)
+        time.sleep(10)
         items = query_sp()
         if not items: continue
         # message(f'Found {len(items)} items')
+
         changes = parse_items(items)
         if not changes: continue
         # message(f'Found {len(items)} changes')
 
-        for (command, userid, name) in changes:
-            res = subprocess.run(command, capture_output=True)
+        for command, userid, name in changes:
+            res = call(command)
             out, err = res.stdout.decode(), res.stderr.decode()
             [message(line) for line in out.splitlines() if line]
             [message('ERR ' + line) for line in err.splitlines() if line]
             if res.returncode: continue
 
-            if name: create_email(name, userid)
-            else: destroy_email(userid)
+            create_email(name, userid) if name else destroy_email(userid)
 
-        on = subprocess.run('virsh list | grep -Poh vm[0-9]+', shell=True, capture_output=True).stdout.decode().strip()
-        message(f'Currently   active {len(on.splitlines())}')
+        on = call('virsh list | grep -Poh vm[0-9]+').stdout.decode().strip()
+        message(f'Currently active {len(on.splitlines())}')
 
 except Exception as ex:
     message(ex)
